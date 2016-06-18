2694ca39ff65ef3baedd61183acf617e8afe85b4
Paloma <paloma_vg@yahoo.com>
2016-06-18 10:08:45 -0500

    correct data

--- src/SurveyApp/Controllers/SurveyController.cs
+++ src/SurveyApp/Controllers/SurveyController.cs
@@ -32,29 +32,98 @@ namespace SurveyApp.Controllers
                 return BadRequest(ModelState);
             }
 
+            var surveyList = _context.Survey.ToList();
+            var questionList = _context.Question.ToList();
+            var answerList = _context.Answer.ToList();
+
+            return Ok(surveyList);
+
             //IQueryable<Survey> survey = from s in _context.Survey
             //select s;
 
-            IQueryable<object> survey = from s in _context.Survey
-                                        join que in _context.Question
-                                        on s.SurveyId equals que.SurveyId
-                                        join ans in _context.Answer
-                                        on que.QuestionId equals ans.QuestionId
-                                        select new
-                                        {
-                                            Name = s.Name,
-                                            Question = que.QuestionText,
-                                            QuestionId = que.QuestionId,
-                                            Answer = ans.AnswerText,
-                                            AnswerId = ans.AnswerId
-                                        };
+            //IQueryable<SurveyQuestion> surveyList = from s in _context.Survey
+            //                                join que in _context.Question
+            //                                on s.SurveyId equals que.SurveyId
+            //                                join ans in _context.Answer
+            //                                on que.QuestionId equals ans.QuestionId
+            //                                select new SurveyQuestion
+            //                                {
+            //                                    SurveyName = s.Name,
+            //                                    QuestionText = que.QuestionText,
+            //                                    QuestionID = que.QuestionId,
+            //                                    AnswerText = ans.AnswerText,
+            //                                    AnswerID = ans.AnswerId
+            //                                };
 
-            if (survey == null)
-            {
-                return NotFound();
-            }
+            //var surveyList = (from s in _context.Survey
+            //                    join que in _context.Question
+            //                    on s.SurveyId equals que.SurveyId
+            //                    join ans in _context.Answer
+            //                    on que.QuestionId equals ans.QuestionId
+            //                    group new { s, que, ans } by que.SurveyId into questionGroup
+            //                    select questionGroup).ToList();
 
-            return Ok(survey);
+            //foreach(var survey in surveyList)
+            //{
+            //    foreach(var item in survey)
+            //    {
+            //        item.
+            //    }
+            //}
+
+            //var surveyList = _context.Survey.ToList();
+            //var questionList = _context.Question.ToList();
+            //var answerList = _context.Answer.ToList();
+
+            //List<SurveyQuestion> surveyQuestions = new List<SurveyQuestion>();
+            //foreach(var survey in surveyList)
+            //{
+            //    foreach(var question in questionList.Where(x => x.SurveyId == survey.SurveyId))
+            //    {
+            //        SurveyQuestion surveyQuestion = new SurveyQuestion
+            //        {
+            //            Survey = survey,
+            //            Question = question
+            //        };
+            //        foreach(var answer in answerList.Where(x => x.QuestionId == question.QuestionId))
+            //        {
+            //            surveyQuestion.Answers.Add(answer);
+            //        }
+            //        surveyQuestions.Add(surveyQuestion);
+            //    }
+            //}
+
+            //List<QuestionsAndAnswers> questionAnswerList = new List<QuestionsAndAnswers>();
+
+            //foreach(var survey in _context.Survey)
+            //{
+            //    foreach(var question in _context.Question.Where(x => x.SurveyId == survey.SurveyId))
+            //    {
+            //        QuestionsAndAnswers questionAnswers = new QuestionsAndAnswers { SurveyQuestion = question };
+            //        foreach(var answer in _context.Answer.Where(x => x.QuestionId == question.QuestionId))
+            //        {
+            //            questionAnswers.Answers.Add(answer);
+            //        }
+
+            //        questionAnswerList.Add(questionAnswers);
+            //    }
+            //}
+
+            //if (surveyList == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //foreach(var question in survey.QuestionList)
+            //{
+            //    questionAnswerList.Add(new QuestionsAndAnswers
+            //    {
+            //        SurveyQuestion = question,
+            //        Answers = question.AnswerList.ToList()
+            //    });
+            //}
+
+            //return Ok(surveyQuestions);
         }
 
         // GET api/values/5
---
+++ src/SurveyApp/Models/SurveyQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyApp.Models
+{
+    public class SurveyQuestion
+    {
+        public Survey Survey {get;set;}
+        public Question Question { get; set; }
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+    }
+}
